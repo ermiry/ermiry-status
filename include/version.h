ed6406acb7217b4c8fe3dd699410447ad494b960
ermiry <erick.salas@ermiry.com>
2021-03-31 23:38:17 -0600

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef _ERMIRY_VERSION_H_
 #define _ERMIRY_VERSION_H_
 
-#define ERMIRY_VERSION                    	"0.1"
-#define ERMIRY_VERSION_NAME               	"Version 0.1"
-#define ERMIRY_VERSION_DATE			    	"18/03/2021"
-#define ERMIRY_VERSION_TIME			    	"23:24 CST"
-#define ERMIRY_VERSION_AUTHOR			    "Erick Salas"
+#define ERMIRY_VERSION					"0.1"
+#define ERMIRY_VERSION_NAME				"Version 0.1"
+#define ERMIRY_VERSION_DATE				"31/03/2021"
+#define ERMIRY_VERSION_TIME				"23:37 CST"
+#define ERMIRY_VERSION_AUTHOR			"Erick Salas"
 
 // print full ermiry version information
 extern void ermiry_version_print_full (void);
