b5056a918cc5e91ab2084f075ae5008a0f2277af
ermiry <erick.salas@ermiry.com>
2021-03-31 23:36:08 -0600

    Added catch all handler in service routes

--- include/routes/service.h
+++ include/routes/service.h
@@ -1,6 +1,9 @@
 #ifndef _ERMIRY_ROUTES_SERVICE_H_
 #define _ERMIRY_ROUTES_SERVICE_H_
 
+struct _HttpReceive;
+struct _HttpRequest;
+
 // GET /api/ermiry
 extern void ermiry_handler (
 	const struct _HttpReceive *http_receive,
@@ -9,6 +12,12 @@ extern void ermiry_handler (
 
 // GET /api/ermiry/version
 extern void ermiry_version_handler (
+	const struct _HttpReceive *http_receive,
+	const struct _HttpRequest *request
+);
+
+// GET *
+extern void ermiry_catch_all_handler (
 	const struct _HttpReceive *http_receive,
 	const struct _HttpRequest *request
 );
