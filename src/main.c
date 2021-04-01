703e07cc98e38aac4fd9c9c6fceb7d7f88b1498c
ermiry <erick.salas@ermiry.com>
2021-03-31 23:36:57 -0600

    Refactored HTTP configuration in main source

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@
 #include <cerver/http/http.h>
 #include <cerver/http/route.h>
 
-#include <cerver/utils/utils.h>
 #include <cerver/utils/log.h>
+#include <cerver/utils/utils.h>
 
 #include "ermiry.h"
 #include "version.h"
@@ -42,107 +42,14 @@ void end (int dummy) {
 static void ermiry_set_ermiry_routes (HttpCerver *http_cerver) {
 
 	/* register top level route */
-	// GET /api/ermiry
-	HttpRoute *ermiry_route = http_route_create (REQUEST_METHOD_GET, "api/ermiry", ermiry_handler);
+	// GET /api/status
+	HttpRoute *ermiry_route = http_route_create (REQUEST_METHOD_GET, "api/status", ermiry_handler);
 	http_cerver_route_register (http_cerver, ermiry_route);
 
 	/* register ermiry children routes */
-	// GET api/ermiry/version
+	// GET api/status/version
 	HttpRoute *ermiry_version_route = http_route_create (REQUEST_METHOD_GET, "version", ermiry_version_handler);
 	http_route_child_add (ermiry_route, ermiry_version_route);
-
-	// GET api/ermiry/auth
-	HttpRoute *ermiry_auth_route = http_route_create (REQUEST_METHOD_GET, "auth", ermiry_auth_handler);
-	http_route_set_auth (ermiry_auth_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (ermiry_auth_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, ermiry_auth_route);
-
-	/*** transactions ***/
-
-	// GET api/ermiry/transactions
-	HttpRoute *transactions_route = http_route_create (REQUEST_METHOD_GET, "transactions", ermiry_transactions_handler);
-	http_route_set_auth (transactions_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (transactions_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, transactions_route);
-
-	// POST api/ermiry/transactions
-	http_route_set_handler (transactions_route, REQUEST_METHOD_POST, ermiry_transaction_create_handler);
-
-	// GET api/ermiry/transactions/:id
-	HttpRoute *single_trans_route = http_route_create (REQUEST_METHOD_GET, "transactions/:id", ermiry_transaction_get_handler);
-	http_route_set_auth (single_trans_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (single_trans_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, single_trans_route);
-
-	// PUT api/ermiry/transactions/:id
-	http_route_set_handler (single_trans_route, REQUEST_METHOD_PUT, ermiry_transaction_update_handler);
-
-	// DELETE api/ermiry/transactions/:id
-	http_route_set_handler (single_trans_route, REQUEST_METHOD_DELETE, ermiry_transaction_delete_handler);
-
-	/*** categories ***/
-
-	// GET api/ermiry/categories
-	HttpRoute *categories_route = http_route_create (REQUEST_METHOD_GET, "categories", ermiry_categories_handler);
-	http_route_set_auth (categories_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (categories_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, categories_route);
-
-	// POST api/ermiry/categories
-	http_route_set_handler (categories_route, REQUEST_METHOD_POST, ermiry_category_create_handler);
-
-	// GET api/ermiry/categories/:id
-	HttpRoute *single_category_route = http_route_create (REQUEST_METHOD_GET, "categories/:id", ermiry_category_get_handler);
-	http_route_set_auth (single_category_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (single_category_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, single_category_route);
-
-	// PUT api/ermiry/categories/:id
-	http_route_set_handler (single_category_route, REQUEST_METHOD_PUT, ermiry_category_update_handler);
-
-	// DELETE api/ermiry/categories/:id
-	http_route_set_handler (single_category_route, REQUEST_METHOD_DELETE, ermiry_category_delete_handler);
-
-	/*** places ***/
-
-	// GET api/ermiry/places
-	HttpRoute *places_route = http_route_create (REQUEST_METHOD_GET, "places", ermiry_places_handler);
-	http_route_set_auth (places_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (places_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, places_route);
-
-	// POST api/ermiry/places
-	http_route_set_handler (places_route, REQUEST_METHOD_POST, ermiry_place_create_handler);
-
-	// GET api/ermiry/places/:id
-	HttpRoute *single_place_route = http_route_create (REQUEST_METHOD_GET, "places/:id", ermiry_place_get_handler);
-	http_route_set_auth (single_place_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
-	http_route_set_decode_data (single_place_route, ermiry_user_parse_from_json, ermiry_user_delete);
-	http_route_child_add (ermiry_route, single_place_route);
-
-	// PUT api/ermiry/places/:id
-	http_route_set_handler (single_place_route, REQUEST_METHOD_PUT, ermiry_place_update_handler);
-
-	// DELETE api/ermiry/places/:id
-	http_route_set_handler (single_place_route, REQUEST_METHOD_DELETE, ermiry_place_delete_handler);
-
-}
-
-static void ermiry_set_users_routes (HttpCerver *http_cerver) {
-
-	/* register top level route */
-	// GET /api/users
-	HttpRoute *users_route = http_route_create (REQUEST_METHOD_GET, "api/users", users_handler);
-	http_cerver_route_register (http_cerver, users_route);
-
-	/* register users children routes */
-	// POST api/users/login
-	HttpRoute *users_login_route = http_route_create (REQUEST_METHOD_POST, "login", users_login_handler);
-	http_route_child_add (users_route, users_login_route);
-
-	// POST api/users/register
-	HttpRoute *users_register_route = http_route_create (REQUEST_METHOD_POST, "register", users_register_handler);
-	http_route_child_add (users_route, users_register_route);
 
 }
 
@@ -166,18 +73,7 @@ static void start (void) {
 		/*** web cerver configuration ***/
 		HttpCerver *http_cerver = (HttpCerver *) ermiry_api->cerver_data;
 
-		http_cerver_auth_set_jwt_algorithm (http_cerver, JWT_ALG_RS256);
-		if (ENABLE_USERS_ROUTES) {
-			http_cerver_auth_set_jwt_priv_key_filename (http_cerver, PRIV_KEY->str);
-		}
-		
-		http_cerver_auth_set_jwt_pub_key_filename (http_cerver, PUB_KEY->str);
-
 		ermiry_set_ermiry_routes (http_cerver);
-
-		if (ENABLE_USERS_ROUTES) {
-			ermiry_set_users_routes (http_cerver);
-		}
 
 		// add a catch all route
 		http_cerver_set_catch_all_route (http_cerver, ermiry_catch_all_handler);
